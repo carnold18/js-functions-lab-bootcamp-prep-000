351e6eb4dd51b5bd5d381f8c3ffa74a680b9b425
Christopher Arnold <chrisarnold2010@gmail.com>
2018-05-31 00:12:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ function happyHolidays() {
   return 'Happy holidays!';
 }
 
-function happyHolidaysTo(you) {
-  return 'Happy holidays, ${name}!';
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
 }
 
-function happyHolidayTo(Independence Day, you) {
-  return 'Happy ${holiday}, ${name}!';
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
 }
 
-function holidayCountdown(20, Mother's Day) {
-  return 'It\'s ${days} days until ${holiday}!';
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`;
 }
